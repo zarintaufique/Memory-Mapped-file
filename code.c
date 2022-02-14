f7198863dac0adbb9c22002b26db7fbc7e38a89f
zarin <81378664+zarintaufique@users.noreply.github.com>
2022-02-14 14:22:03 +0500

    libraires added

---
+++ code.c
@@ -0,0 +1,12 @@
+#include <sys/mman.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <pthread.h>
